2ae29b9cde3556730d20bb96eb0f2254c440f939
Sania Kiran <153180171+trioclab@users.noreply.github.com>
2024-03-28 11:18:08 +0500

    Calculator
    Abi to bs shurwat hai

---
+++ Calculator/main.js
@@ -0,0 +1,31 @@
+// Declaring the names to ids
+
+let upperEl = document.getElementById("upper")
+let signEl = document.getElementById("sign")
+let resultEl = document.getElementById("result")
+
+// Digits Buttons
+
+let  btn9 = getElementById("9")
+let  btn8 = getElementById("8")
+let  btn7 = getElementById("7")
+let  btn6 = getElementById("6")
+let  btn5 = getElementById("5")
+let  btn4 = getElementById("4")
+let  btn3 = getElementById("3")
+let  btn2 = getElementById("2")
+let  btn1 = getElementById("1")
+let  btn0 = getElementById("0")
+
+
+// Clear Button
+
+let  btnClear = getElementById("C")
+
+// Signs
+
+let signPlus = getElementById("+")
+let signMinus = getElementById("-")
+let signMulitply = getElementById("x")
+let signDivide = getElementById("/")
+let signEqual = getElementById("=")
